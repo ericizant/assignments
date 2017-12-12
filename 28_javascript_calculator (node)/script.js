3cac5860c456b7a30ec0b91474882a4726d0cf70
Eric Izant <ericizant@gmail.com>
2017-12-12 14:31:00 -0700

    Adding new files.

---
+++ 28_javascript_calculator (node)/script.js
@@ -0,0 +1,7 @@
+var ask = require("readline-sync")
+
+var firstNum = ask.question("What is your first number?");
+
+var secondNum = ask.question("What is your second number?");
+
+console.log(firstNum, secondNum);
