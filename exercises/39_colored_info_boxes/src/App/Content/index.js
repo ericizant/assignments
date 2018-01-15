53b6eeb1275b50529bde698e956319f3d8b93a5b
Eric Izant <ericizant@gmail.com>
2018-01-15 15:58:42 -0700

    Added a modified version of this exercise

--- exercises/39_colored_info_boxes/src/App/Content/index.js
+++ exercises/39_colored_info_boxes/src/App/Content/index.js
@@ -3,13 +3,11 @@ import React from "react";
 function Content(props) {
     return (
         <div style={props.parentStyle}>
-            {props.text.map((text) => {
-                return <p style={props.childStyle}>{text.title}{text.subtitle}{text.info}</p>;
-            })}
+            <h1>{props.boxProps.title}</h1>
+            <h2>{props.boxProps.subtitle}</h2>
+            <p>{props.boxProps.information}</p>
         </div>
-    
-        
-    )
+    );
 }
 
 export default Content;
--- exercises/39_colored_info_boxes/src/App/index.js
+++ exercises/39_colored_info_boxes/src/App/index.js
@@ -1,69 +1,82 @@
 import React from "react";
 import Content from "./Content";
+import "./index.css";
 
-function App(props) {
-    let text = [{
-        title: "Title: "
-    }, {
-        subtitle: "Subtitle: "
-    }, {
-        info: "Info: "
-    }];
+function App() {
     return (
-        <div>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "red", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}
-                title={{title: "Eric"}}>
+        <div className="app-wrapper">
+            <Content boxProps={{
+                title: "Dog",
+                subtitle: "Mammal",
+                information: "Dogs have four legs and padded paws. They are cute and fun. They bark."
+            }}
+                parentStyle={{ backgroundColor: "red", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "orange", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Cat",
+                subtitle: "Mammal",
+                information: "A small domesticated carnivorous mammal with soft fur, a short snout, and retractile claws. It is widely kept as a pet or for catching mice, and many breeds have been developed."
+            }}
+                parentStyle={{ backgroundColor: "orange", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "yellow", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Betta Fish",
+                subtitle: "Cute, furry animals",
+                information: "The Siamese fighting fish, also sometimes colloquially known as the betta, is a species in the gourami family which is popular as an aquarium fish. Bettas can be territorial fish and are prone to high levels of aggression towards each other."
+            }}
+                parentStyle={{ backgroundColor: "yellow", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "green", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Turtle",
+                subtitle: "Reptile",
+                information: "A slow-moving reptile, enclosed in a scaly or leathery domed shell into which it can retract its head and thick legs."
+            }}
+                parentStyle={{ backgroundColor: "green", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "blue", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Hamster",
+                subtitle: "Rodent",
+                information: "Hamsters are technically rodents but have nonetheless become popular small house pets. Because they are easy to breed in captivity, hamsters are often used as laboratory animals."
+            }}
+                parentStyle={{ backgroundColor: "blue", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "indigo", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Ferret",
+                subtitle: "Mammal",
+                information: "A domesticated polecat kept as a pet or used, especially in Europe, for catching rabbits. It is typically albino or brown."
+            }}
+                parentStyle={{ backgroundColor: "purple", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "violet", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Cobra",
+                subtitle: "Reptile",
+                information: "A highly venomous snake native to Africa and Asia that spreads the skin of its neck into a hood when disturbed."
+            }}
+                parentStyle={{ backgroundColor: "grey", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "brown", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Hermit Crab",
+                subtitle: "Crustacean",
+                information: "A crab with a soft asymmetrical abdomen that lives in a castoff mollusk shell for protection. In several kinds, the shell becomes covered with sponges, sea anemones, or bryozoans."
+            }}
+                parentStyle={{ backgroundColor: "darkslategray", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "black", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Goldfish",
+                subtitle: "Fish",
+                information: "A small reddish-golden Eurasian carp, popular in ponds and aquariums. A long history of breeding in China and Japan has resulted in many varieties of form and color."
+            }}
+                parentStyle={{ backgroundColor: "lightblue", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
-            <Content
-                text={text}
-                parentStyle={{ backgroundColor: "grey", height: "100px", padding: "20px" }}
-                childStyle={{ color: "white", margin: "5px" }}>
+            <Content boxProps={{
+                title: "Guinea Pig",
+                subtitle: "Rodent",
+                information: "A domesticated, tailless South American cavy, originally raised for food. It no longer occurs in the wild and is now typically kept as a pet or for laboratory research."
+            }}
+                parentStyle={{ backgroundColor: "coral", height: "300px", width: "300px", textAlign: "center", padding: "20px" }}>
             </Content>
         </div>
-    )
+    );
 }
 
 export default App;
